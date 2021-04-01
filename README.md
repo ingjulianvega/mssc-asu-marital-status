58f880e8818723c96c8e59bfc0c53577a9cfe15d
Julian Vega <ingjulianvega@gmail.com>
2021-04-01 15:52:21 -0500

    :zap:Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ASU marital status management microservice
+
+### BOM
+```
+Bill Of Materials. A BOM is a special kind of POM that is used to control the versions of a project's dependencies and provide a central place to define and update those versions. 
+BOM provides the flexibility to add a dependency to our module without worrying about the version that we should depend on
+```
+
---
+++ src/main/java/ingjulianvega/ximic/msscasumaritalstatus/MsscAsuMaritalStatusApplication.java
@@ -0,0 +1,13 @@
+package ingjulianvega.ximic.msscasumaritalstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscAsuMaritalStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscAsuMaritalStatusApplication.class, args);
+	}
+
+}
